f516beb756d5e5e45df093b3a74934fc8cf16ada
usamasadiq029 <usamasadiq029@gmail.com>
2023-06-26 03:12:51 +0500

    add commenting

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,27 +16,27 @@ const twoBtn = document.querySelector("#twoBtn");
 const threeBtn = document.querySelector("#threeBtn");
 const zeroBtn = document.querySelector("#zeroBtn");
 
-const substractBtn = document.querySelector("#substractBtn");
+// Operation buttons
+const addBtn = document.querySelector("#addBtn");
+const subtractBtn = document.querySelector("#subtractBtn");
 const multiplyBtn = document.querySelector("#multiplyBtn");
 const divideBtn = document.querySelector("#divideBtn");
-const addBtn = document.querySelector("#addBtn");
-const equalBtn = document.querySelector("#equalBtn");
 const modulusBtn = document.querySelector("#modulusBtn");
 
+// Other buttons
 const acBtn = document.querySelector("#acBtn");
 const cBtn = document.querySelector("#cBtn");
+const equalBtn = document.querySelector("#equalBtn");
 
 /* Handlers */
 
 // Clear Values - Clear all the values from result element
-
 function clearValues() {
   val.value = "";
   result.value = "";
 }
 
 // Clear Integer - Clear the last value from result element
-
 function clearInteger() {
   // Get the current value from the display
   const currentValue = result.value;
@@ -49,13 +49,11 @@ function clearInteger() {
 }
 
 // Set Value - Set value in input field
-
 function setValue(btn) {
   result.value += btn.value;
 }
 
 // Calculate - Calculate all the basic operations based on operator and operands
-
 function calculate(operation, num1, num2) {
   let result;
 
@@ -84,15 +82,18 @@ function calculate(operation, num1, num2) {
 
 let btn = null;
 
-function upadteValue(button) {
+// Update Value - Set the value in 'val' and clear 'result'
+function updateValue(button) {
   val.value = result.value;
   result.value = "";
 
+  // Store the selected operation for calculation
   btn = button.value;
 }
 
+// Output - Perform the calculation and update the result value
 function output() {
-  // Call the calculate function with the operation and operands
+  // Call the calculate function with the selected operation and operands
   const calculation = calculate(btn, Number(val.value), Number(result.value));
 
   // Remove the hold value
@@ -102,11 +103,15 @@ function output() {
   result.value = calculation;
 }
 
+/* Event Listeners */
+
 // Add event listeners to each button
 
+// Clear buttons
 acBtn.addEventListener("click", clearValues);
 cBtn.addEventListener("click", clearInteger);
 
+// Numeric buttons
 sevenBtn.addEventListener("click", () => setValue(sevenBtn));
 eightBtn.addEventListener("click", () => setValue(eightBtn));
 nineBtn.addEventListener("click", () => setValue(nineBtn));
@@ -118,10 +123,12 @@ twoBtn.addEventListener("click", () => setValue(twoBtn));
 threeBtn.addEventListener("click", () => setValue(threeBtn));
 zeroBtn.addEventListener("click", () => setValue(zeroBtn));
 
-addBtn.addEventListener("click", () => upadteValue(addBtn));
-substractBtn.addEventListener("click", () => upadteValue(substractBtn));
-multiplyBtn.addEventListener("click", () => upadteValue(multiplyBtn));
-divideBtn.addEventListener("click", () => upadteValue(divideBtn));
-modulusBtn.addEventListener("click", () => upadteValue(modulusBtn));
+// Operation buttons
+addBtn.addEventListener("click", () => updateValue(addBtn));
+subtractBtn.addEventListener("click", () => updateValue(subtractBtn));
+multiplyBtn.addEventListener("click", () => updateValue(multiplyBtn));
+divideBtn.addEventListener("click", () => updateValue(divideBtn));
+modulusBtn.addEventListener("click", () => updateValue(modulusBtn));
 
+// Equal button
 equalBtn.addEventListener("click", output);
